dbc47f6071c423a8ff5e5f25873876fb33a12a87
Manuel Bugallo <marvinody@gmail.com>
2024-05-11 02:35:03 -0400

    adding events and colors

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -3,47 +3,260 @@ const {
   Interval,
 } = luxon
 
+const TOTAL_COLORS = 18;
+
+const pickColorClass = (idx) => {
+  const count = idx % TOTAL_COLORS;
+  return `bg-${count}`;
+}
+
 const dummySales = [
   {
-    name: "Test Calendar event",
-    image: "/default.jpg",
-    desc: "This is a test event that spans multiple months and has a long description. This is AI generated text and is not meant to be read by humans.",
+    name: "Fumo Sale 2022 #1",
+    image: "/images/sales/2022-01.jpg",
+    desc: "First time we see a Lost Word collab, giving us Dark Youmu. The sale also included Reimu & Marisa.",
     buying: {
-      begin: '2024-04-14',
-      end: '2024-05-18',
+      begin: '2022-05-27',
+      end: '2022-06-15',
     },
     shipping: {
-      begin: '2024-04-19',
+      begin: '2022-11-12',
+      end: '2022-11-30',
+    }
+  }, {
+    name: "Fumo Sale 2022 #2",
+    image: "/images/sales/2022-02.jpg",
+    desc: "Second sale of 2022, featuring Alice, Patchy, Kokoro, and Tenshi.",
+    buying: {
+      begin: '2022-06-22',
+      end: '2022-07-11',
+    },
+    shipping: {
+      begin: '2023-12-10',
+      end: '2023-12-31',
+    }
+  }, {
+    name: "Fumo Sale 2022 #3",
+    image: "/images/sales/2022-03.jpg",
+    desc: "Third sale of 2022, featuring Deka releases of Youmu, Yuyuko, Remilia, Flandre & Sakuya. This end date isn't correct and went on until no more supplies.",
+    buying: {
+      begin: '2022-07-27',
+      end: '2022-07-31',
+    },
+    shipping: {
+      begin: '2022-10-11',
+      end: '2023-10-31',
+    }
+  }, {
+    name: "Fumo Sale 2022 #4",
+    image: "/images/sales/2022-04.jpg",
+    desc: "Fourth sale of 2022, featuring Deka releases of Reisen (blazer), Tewi, Kaguya, and Mokou.",
+    buying: {
+      begin: '2022-08-24',
+      end: '2022-09-21',
+    },
+    shipping: {
+      begin: '2023-02-17',
+      end: '2023-02-28',
+    }
+  }, {
+    name: "Fumo Sale 2022 #5",
+    image: "/images/sales/2022-05.jpg",
+    desc: "Fifth sale of 2022, featuring Youmu, Yuyuko, Shion & Jo'on.",
+    buying: {
+      begin: '2022-09-28',
+      end: '2022-10-26',
+    },
+    shipping: {
+      begin: '2023-03-27',
+      end: '2023-04-03',
+    }
+  }, {
+    name: "Fumo Sale 2022 #6",
+    image: "/images/sales/2022-06.jpg",
+    desc: "Sixth sale of 2022, featuring Junko, Keiki, Aya, Meiling, Suwako and Mannaka Satori, Koishi, Cirno.",
+    buying: {
+      begin: '2022-10-24',
+      end: '2022-11-21',
+    },
+    shipping: {
+      begin: '2023-04-15',
+      end: '2023-04-30',
+    }
+  }, {
+    name: "Fumo Sale 2022 #7",
+    image: "/images/sales/2022-07.jpg",
+    desc: "Seventh sale of 2022, featuring Deka releases of Deka releases of Alice, Sanae, Sakuya, Youmu, and Yuyuko.",
+    buying: {
+      begin: '2022-12-07',
+      end: '2022-12-31',
+    },
+    shipping: {
+      begin: '2023-06-13',
+      end: '2023-06-20',
+    }
+  }, {
+    name: "Fumo Sale 2023 #1",
+    image: "/images/sales/2023-01.jpg",
+    desc: "First sale of 2023, featuring of Sakuya, Remilia, and Flandre.",
+    buying: {
+      begin: '2023-02-10',
+      end: '2023-03-13',
+    },
+    shipping: {
+      begin: '2023-08-24',
+      end: '2023-08-31',
+    }
+  }, {
+    name: "Fumo Sale 2023 #2",
+    image: "/images/sales/2023-02.jpg",
+    desc: "Second sale of 2023, featuring of Rumia, Eiki, and Nitori.",
+    buying: {
+      begin: '2023-03-10',
+      end: '2023-04-10',
+    },
+    shipping: {
+      begin: '2023-09-20',
+      end: '2023-09-30',
+    }
+  }, {
+    name: "Fumo Sale 2023 #3",
+    image: "/images/sales/2023-03.jpg",
+    desc: "Third sale of 2023, featuring of Renko, Maribel, Suika, Futo, Seija, Miko, & Mannaka Yuyuko and Youmu.",
+    buying: {
+      begin: '2023-05-08',
+      end: '2023-05-29',
+    },
+    shipping: {
+      begin: '2023-11-21',
+      end: '2023-11-30',
+    }
+  }, {
+    name: "Fumo Sale 2023 #4",
+    image: "/images/sales/2023-04.jpg",
+    desc: "Fourth sale of 2023, featuring Lost Word recolors of Reimu & Marisa.",
+    buying: {
+      begin: '2023-07-02',
+      end: '2023-08-28',
+    },
+    shipping: {
+      begin: '2024-01-23',
+      end: '2024-01-31',
+    }
+  }, {
+    name: "Fumo Sale 2023 #5",
+    image: "/images/sales/2023-05.jpg",
+    desc: "Fifth sale of 2023, featuring of Rinnosuke, Yukari & Patchy.",
+    buying: {
+      begin: '2023-08-10',
+      end: '2023-09-14',
+    },
+    shipping: {
+      begin: '2024-02-20',
+      end: '2024-02-28',
+    }
+  }, {
+    name: "Fumo Sale 2023 #6",
+    image: "/images/sales/2023-06.jpg",
+    desc: "Sixth sale of 2023, featuring of Cirnos, regular + suntanned.",
+    buying: {
+      begin: '2023-09-01',
+      end: '2023-10-02',
+    },
+    shipping: {
+      begin: '2024-04-17',
       end: '2024-04-30',
+    }
+  }, {
+    name: "Fumo Sale 2023 #7",
+    image: "/images/sales/2023-07.jpg",
+    desc: "Seventh sale of 2023, featuring of PC98 Reimu & Marisa, Lost Word Remilia, Flandre & other Flandre, and Mannaka Patchy & Alice",
+    buying: {
+      begin: '2023-11-13',
+      end: '2023-12-11',
+    },
+    shipping: {
+      begin: '2024-06-15',
+      end: '2024-06-30',
+    }
+  }, {
+    name: "Fumo Sale 2023 #8",
+    image: "/images/sales/2023-08.jpg",
+    desc: "Eighth sale of 2023, featuring Satori, Koishi & Sanae.",
+    buying: {
+      begin: '2023-12-08',
+      end: '2024-01-10',
+    },
+    shipping: {
+      begin: '2024-07-10',
+      end: '2024-07-31',
+    }
+  }, {
+    name: "Fumo Sale 2024 #1",
+    image: "/images/sales/2024-01.jpg",
+    desc: "First sale of 2024, featuring Kasen, Eirin & Yuuka",
+    buying: {
+      begin: '2024-01-09',
+      end: '2024-02-12',
+    },
+    shipping: {
+      begin: '2024-08-10',
+      end: '2024-08-31',
+    }
+  }, {
+    name: "Fumo Sale 2024 #2",
+    image: "/images/sales/2024-02.jpg",
+    desc: "Second sale of 2024, featuring of Reisen (no blazer), Aya, Hatate, & Momiji",
+    buying: {
+      begin: '2024-02-09',
+      end: '2024-03-11',
+    },
+    shipping: {
+      begin: '2024-09-20',
+      end: '2024-09-30',
+    }
+  }, {
+    name: "Fumo Sale 2024 #3",
+    image: "/images/sales/2024-03.jpg",
+    desc: "Third sale of 2024, featuring of Parsee, Orin, Okuu, Kogasa, Nue, & Sagume, and Mannaka Remilia, Flandre, & Sakuya.",
+    buying: {
+      begin: '2024-05-07',
+      end: '2024-06-10',
+    },
+    shipping: {
+      begin: '2024-11-20',
+      end: '2024-11-30',
     }
   }
 ];
 
 const dummyEvents = [
-  ...dummySales.flatMap((sale) => {
+  ...dummySales.flatMap((sale, idx) => {
     return [
       {
-        name: sale.name + ' buying',
+        name: sale.name + ' - Buying',
         start: sale.buying.begin,
         end: sale.buying.end,
         image: sale.image,
         desc: sale.desc,
         type: 'sale',
+        idx,
       },
       {
-        name: sale.name + ' shipping',
+        name: sale.name + ' - Shipping',
         start: sale.shipping.begin,
         end: sale.shipping.end,
         image: sale.image,
         desc: sale.desc,
         type: 'shipping',
+        idx,
       }
     ]
   })
 ];
 
 const slugify = (text) => {
-  return text.toLowerCase().replace(/ /g, '-');
+  return text.toLowerCase().replace(/ /g, '-').replace(/#/g, '');
 }
 
 
@@ -141,6 +354,8 @@ const renderCalendar = (range) => {
         .attr('rel', 'modal:open')
         .attr('href', '#' + slugify(event.name))
         .append(eventText);
+
+      eventDiv.addClass(pickColorClass(event.idx));
 
       eventDiv.append(eventLink);
     
