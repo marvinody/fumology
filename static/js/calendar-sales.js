a2c55f30fece640ae85eeccffa54bde15ac55ee3
Your Name <you@example.com>
2026-02-15 04:10:37 -0500

    feat: update calendar listings with super information for data

--- static/js/calendar-sales.js
+++ static/js/calendar-sales.js
@@ -3,6 +3,14 @@ window.dummySales = [
     name: "Fumo Sale 2022 #1",
     image: "/images/sales/2022-01.jpg",
     desc: "First time we see a Lost Word collab, giving us Dark Youmu. The sale also included Reimu & Marisa.",
+    merch: [
+      {
+        chars: ["Youmu Konpaku", "Reimu Hakurei", "Marisa Kirisame"],
+        price: 4950,
+        type: "20cm",
+        MTO: true,
+      },
+    ],
     buying: {
       begin: "2022-05-27",
       end: "2022-06-15",
@@ -16,6 +24,14 @@ window.dummySales = [
     name: "Fumo Sale 2022 #2",
     image: "/images/sales/2022-02.jpg",
     desc: "Second sale of 2022, featuring Alice, Patchy, Kokoro, and Tenshi.",
+    merch: [
+      {
+        chars: ["Alice Margatroid", "Patchouli Knowledge", "Hata no Kokoro", "Tenshi Hinanawi"],
+        price: 4950,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2022-06-22",
       end: "2022-07-11",
@@ -29,6 +45,19 @@ window.dummySales = [
     name: "Fumo Sale 2022 #3",
     image: "/images/sales/2022-03.jpg",
     desc: "Third sale of 2022, featuring Deka releases of Youmu, Yuyuko, Remilia, Flandre & Sakuya. This end date isn't correct and went on until no more supplies.",
+    merch: [
+      {
+        chars: ["Youmu Konpaku", "Yuyuko Saigyouji"],
+        price: 55_000,
+        type: "Deka",
+        MTO: false,
+      }, {
+        chars: ["Remilia Scarlet", "Flandre Scarlet", "Sakuya Izayoi"],
+        price: 49_500,
+        type: "Deka",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2022-07-27",
       end: "2022-07-31",
@@ -42,6 +71,14 @@ window.dummySales = [
     name: "Fumo Sale 2022 #4",
     image: "/images/sales/2022-04.jpg",
     desc: "Fourth sale of 2022, featuring Deka releases of Reisen (blazer), Tewi, Kaguya, and Mokou.",
+    merch: [
+      {
+        chars: ["Reisen Udongein Inaba", "Tewi Inaba", "Kaguya Houraisan", "Fujiwara no Mokou"],
+        price: 4950,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2022-08-24",
       end: "2022-09-21",
@@ -55,6 +92,14 @@ window.dummySales = [
     name: "Fumo Sale 2022 #5",
     image: "/images/sales/2022-05.jpg",
     desc: "Fifth sale of 2022, featuring Youmu, Yuyuko, Shion & Jo'on.",
+    merch: [
+      {
+        chars: ["Youmu Konpaku", "Yuyuko Saigyouji", "Shion Yorigami", "Jo'on Yorigami"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2022-09-28",
       end: "2022-10-26",
@@ -68,6 +113,22 @@ window.dummySales = [
     name: "Fumo Sale 2022 #6",
     image: "/images/sales/2022-06.jpg",
     desc: "Sixth sale of 2022, featuring Junko, Keiki, Aya, Meiling, Suwako and Mannaka Satori, Koishi, Cirno.",
+    merch: [
+      {
+        chars: ["Junko", "Keiki Haniyasushin", "Aya Shameimaru", "Hong Meiling", "Suwako Moriya"],
+        price: 4950,
+        type: "20cm",
+        MTO: true,
+      }, {
+        chars: ["Satori Komeiji", "Koishi Komeiji", "Cirno"],
+        type: "40cm",
+        MTO: true,
+      }, {
+        chars: ["Youmu Konpaku", "Yuyuko Saigyouji", "Alice Margatroid", "Patchouli Knowledge", "Koishi Komeiji", "Satori Komeiji"],
+        type: "mini",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2022-10-24",
       end: "2022-11-21",
@@ -81,6 +142,14 @@ window.dummySales = [
     name: "Fumo Sale 2022 #7",
     image: "/images/sales/2022-07.jpg",
     desc: "Seventh sale of 2022, featuring Deka releases of Deka releases of Alice, Sanae, Sakuya, Youmu, and Yuyuko.",
+    merch: [
+      {
+        chars: ["Alice Margatroid", "Sanae Kochiya", "Sakuya Izayoi", "Youmu Konpaku", "Yuyuko Saigyouji"],
+        price: 55_000,
+        type: "Deka",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2022-12-07",
       end: "2022-12-31",
@@ -94,6 +163,14 @@ window.dummySales = [
     name: "Fumo Sale 2023 #1",
     image: "/images/sales/2023-01.jpg",
     desc: "First sale of 2023, featuring of Sakuya, Remilia, and Flandre.",
+    merch: [
+      {
+        chars: ["Sakuya Izayoi", "Remilia Scarlet", "Flandre Scarlet"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2023-02-10",
       end: "2023-03-13",
@@ -107,6 +184,14 @@ window.dummySales = [
     name: "Fumo Sale 2023 #2",
     image: "/images/sales/2023-02.jpg",
     desc: "Second sale of 2023, featuring of Rumia, Eiki, and Nitori.",
+    merch: [
+      {
+        chars: ["Rumia", "Eiki Shiki, Yamaxanadu", "Nitori Kawashiro"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2023-03-10",
       end: "2023-04-10",
@@ -120,6 +205,22 @@ window.dummySales = [
     name: "Fumo Sale 2023 #3",
     image: "/images/sales/2023-03.jpg",
     desc: "Third sale of 2023, featuring of Renko, Maribel, Suika, Futo, Seija, Miko, & Mannaka Yuyuko and Youmu.",
+    merch: [
+      {
+        chars: ["Renko Usami", "Maribel Hearn", "Suika Ibuki", "Mononobe no Futo", "Seija Kijin", "Toyosatomimi no Miko"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }, {
+        chars: ["Yuyuko Saigyouji", "Youmu Konpaku"],
+        type: "40cm",
+        MTO: true,
+      }, {
+        chars: ["Sanae Kochiya", "Yukari Yakumo", "Ran Yakumo", "Chen"],
+        type: "mini",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2023-05-08",
       end: "2023-05-29",
@@ -133,6 +234,14 @@ window.dummySales = [
     name: "Fumo Sale 2023 #4",
     image: "/images/sales/2023-04.jpg",
     desc: "Fourth sale of 2023, featuring Lost Word recolors of Reimu & Marisa.",
+    merch: [
+      {
+        chars: ["Reimu Hakurei", "Marisa Kirisame"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2023-07-02",
       end: "2023-08-28",
@@ -146,6 +255,14 @@ window.dummySales = [
     name: "Fumo Sale 2023 #5",
     image: "/images/sales/2023-05.jpg",
     desc: "Fifth sale of 2023, featuring of Rinnosuke, Yukari & Patchy.",
+    merch: [
+      {
+        chars: ["Rinnosuke Morichika", "Yukari Yakumo", "Patchouli Knowledge"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2023-08-10",
       end: "2023-09-14",
@@ -159,6 +276,14 @@ window.dummySales = [
     name: "Fumo Sale 2023 #6",
     image: "/images/sales/2023-06.jpg",
     desc: "Sixth sale of 2023, featuring of Cirnos, regular + suntanned.",
+    merch: [
+      {
+        chars: ["Cirno"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2023-09-01",
       end: "2023-10-02",
@@ -172,6 +297,28 @@ window.dummySales = [
     name: "Fumo Sale 2023 #7",
     image: "/images/sales/2023-07.jpg",
     desc: "Seventh sale of 2023, featuring of PC98 Reimu & Marisa, Lost Word Remilia, Flandre & other Flandre, and Mannaka Patchy & Alice",
+    merch: [
+      {
+        chars: [{
+          name: "Reimu Hakurei",
+          variants: ["PC98"],
+        },{
+          name: "Marisa Kirisame",
+          variants: ["PC98"],
+        },"Remilia Scarlet", "Flandre Scarlet"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }, {
+        chars: ["Patchouli Knowledge", "Alice Margatroid"],
+        type: "40cm",
+        MTO: true,
+      }, {
+        chars: ["Reisen Udongein Inaba", "Cirno", "Aya Shameimaru", "Shanghai"],
+        type: "mini",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2023-11-13",
       end: "2023-12-11",
@@ -185,6 +332,14 @@ window.dummySales = [
     name: "Fumo Sale 2023 #8",
     image: "/images/sales/2023-08.jpg",
     desc: "Eighth sale of 2023, featuring Satori, Koishi & Sanae.",
+      merch: [
+      {
+        chars: ["Satori Komeiji", "Koishi Komeiji", "Sanae Kochiya"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2023-12-08",
       end: "2024-01-10",
@@ -198,6 +353,14 @@ window.dummySales = [
     name: "Fumo Sale 2024 #1",
     image: "/images/sales/2024-01.jpg",
     desc: "First sale of 2024, featuring Kasen, Eirin & Yuuka",
+    merch: [
+      {
+        chars: ["Kasen Ibaraki", "Eirin Yagokoro", "Yuuka Kazami"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2024-01-09",
       end: "2024-02-12",
@@ -211,6 +374,14 @@ window.dummySales = [
     name: "Fumo Sale 2024 #2",
     image: "/images/sales/2024-02.jpg",
     desc: "Second sale of 2024, featuring of Reisen (no blazer), Aya, Hatate, & Momiji",
+    merch: [
+      {
+        chars: ["Reisen Udongein Inaba", "Aya Shameimaru", "Hatate Himekaidou", "Momiji Inubashiri"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2024-02-09",
       end: "2024-03-11",
@@ -224,6 +395,22 @@ window.dummySales = [
     name: "Fumo Sale 2024 #3",
     image: "/images/sales/2024-03.jpg",
     desc: "Third sale of 2024, featuring of Parsee, Orin, Okuu, Kogasa, Nue, & Sagume, and Mannaka Remilia, Flandre, & Sakuya.",
+    merch: [
+      {
+        chars: ["Parsee Mizuhashi", "Orin", "Utsuho Reiuji", "Kogasa Tatara", "Nue Houjuu", "Sagume Kishin"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }, {
+        chars: ["Remilia Scarlet", "Flandre Scarlet", "Sakuya Izayoi"],
+        type: "40cm",
+        MTO: true,
+      }, {
+        chars: ["Fujiwara no Mokou", "Tenshi Hinanawi", "Hata no Kokoro", "Suwako Moriya"],
+        type: "mini",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2024-05-07",
       end: "2024-06-10",
@@ -237,6 +424,14 @@ window.dummySales = [
     name: "Fumo Sale 2024 #4",
     image: "/images/sales/2024-04.jpg",
     desc: "Fourth sale of 2024, featuring of Ran, Chen, & Yukari",
+    merch: [
+      {
+        chars: ["Ran Yakumo", "Chen", "Yukari Yakumo"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2024-06-06",
       end: "2024-07-16",
@@ -249,7 +444,14 @@ window.dummySales = [
   {
     name: "Fumo Sale 2024 #5",
     image: "/images/sales/2024-05.jpg",
-    desc: "Fifth sale of 2024, featuring Mannaka Satori & Koishi ",
+    desc: "Fifth sale of 2024, featuring Mannaka Satori & Koishi",
+    merch: [
+      {
+        chars: ["Satori Komeiji", "Koishi Komeiji"],
+        type: "40cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2024-07-25",
       end: "2024-08-28",
@@ -262,7 +464,20 @@ window.dummySales = [
   {
     name: "Fumo Sale 2024 #6",
     image: "/images/sales/2024-06.jpg",
-    desc: "Sixth sale of 2024, featuring Hecc, Okina, Chimata, Hina & Nazrin ",
+    desc: "Sixth sale of 2024, featuring Hecc, Okina, Chimata, Hina & Nazrin",
+    merch: [
+      {
+        chars: ["Hecatia Lapislazuli", "Okina Matara", "Chimata Tenkyuu", "Hina Kagiyama", "Nazrin"],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      },
+      {
+        chars: ["Reimu Hakurei", "Marisa Kirisame", "Reisen Udongein Inaba", "Aya Shameimaru"],
+        type: "40cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2024-10-21",
       end: "2024-11-25",
@@ -276,6 +491,23 @@ window.dummySales = [
     name: "Fumo Sale 2024 #7.5",
     image: "/images/sales/2024-07-5.jpg",
     desc: "Seventh sale of 2024, featuring Lostword variants of Yuyuko, Sakuya, and Koishi",
+    merch: [
+      {
+        chars: [{
+          name: "Yuyuko Saigyouji",
+          variants: ["Lostword"]
+        }, {
+          name: "Sakuya Izayoi",
+          variants: ["Lostword"]
+        }, {
+          name: "Koishi Komeiji",
+          variants: ["Lostword"]
+        }],
+        price: 5500,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2024-07-05",
       end: "2024-08-28",
@@ -288,7 +520,18 @@ window.dummySales = [
   {
     name: "Fumo Sale 2024 #7",
     image: "/images/sales/2024-07.jpg",
-    desc: "Mannaka only MTO sale of 2024, featuring Cirno and Tan Cirno",
+    desc: "Deka MTO sale of 2024, featuring Cirno and Tan Cirno",
+      merch: [
+      {
+        chars: [{
+          name: "Cirno",
+          variants: ["Regular", "Suntanned"]
+        }],
+        price: 66_000,
+        type: "Deka",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2024-11-01",
       end: "2024-11-25",
@@ -302,6 +545,23 @@ window.dummySales = [
     name: "Fumo Sale 2024 #8",
     image: "/images/sales/2024-08.jpg",
     desc: "Eighth sale of 2024, featuring new Kourindou Youmu with resale of Kourindou Reimu & Marisa",
+    merch: [
+      {
+        chars: [{
+          name: "Youmu Konpaku",
+          variants: ["Kourindou"]
+        }, {
+          name: "Reimu Hakurei",
+          variants: ["Kourindou"]
+        }, {
+          name: "Marisa Kirisame",
+          variants: ["Kourindou"]
+        }],
+        price: 6600,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2024-11-22",
       end: "2024-12-23",
@@ -315,6 +575,21 @@ window.dummySales = [
     name: "Fumo Sale 2026 #1a",
     image: "/images/sales/HAwrUC6aAAUrmvt.jpg",
     desc: "",
+    merch: [
+      {
+        chars: ["Youmu Konpaku", "Fujiwara no Mokou", "Kaguya Houraisan", "Rinnosuke Morichika", {
+          name: "Patchouli Knowledge",
+          variants: ["Kourindou"]
+        }, 'Shion Yorigami', 'Jo\'on Yorigami'],
+        price: 6600,
+        type: "20cm",
+        MTO: true,
+      }, {
+        chars: ['Satori Komeiji', 'Koishi Komeiji'],
+        type: "mini",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2026-02-09",
       end: "2026-03-16",
@@ -329,6 +604,13 @@ window.dummySales = [
     name: "Mini EoSD Gang",
     image: "/images/sales/HAwlOzKaAAMsjy4.jpg",
     desc: "17cm variants of Remilia, Flandre, and Sakuya",
+    merch: [
+      {
+        chars: ["Remilia Scarlet", "Flandre Scarlet", "Sakuya Izayoi"],
+        type: "17cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2026-02-09",
       end: "2026-03-16",
@@ -343,6 +625,13 @@ window.dummySales = [
     name: "Reimu + Marisa with seat",
     image: "/images/sales/HAtGakFaAAAQXMG.jpg",
     desc: "Reimu and Marisa sets with colored chairs",
+    merch: [
+      {
+        chars: ["Reimu Hakurei", "Marisa Kirisame"],
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2026-02-09",
       end: "2026-03-16",
@@ -371,6 +660,13 @@ window.dummySales = [
     name: "Chair Sale",
     image: "/images/sales/G9Yv4sgagAAU4kX.jpg",
     desc: "Limited time NOT MTO chair sale",
+    merch: [
+      {
+        items: ["Gift Plush Stand"],
+        type: '20cm',
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2025-12-30",
       end: "2026-02-09",
@@ -385,6 +681,30 @@ window.dummySales = [
     name: "Clownpiece and gang MTO Sale ",
     image: "/images/sales/G3R6gp9aQAAJuN9.jpg",
     desc: "Yakumo Family 40cm\nDaiyousei\nReimu & Marisa alternate color\nKoakuma\nClownpiece!\nTsukasa\nMegumu",
+    merch: [
+      {
+        chars: ["Ran Yakumo", "Chen", "Yukari Yakumo"],
+        type: "40cm",
+        MTO: true,
+      },
+      {
+        chars: ["Daiyousei", "Koakuma", "Clownpiece", "Tsukasa", "Megumu", {
+          name: "Reimu Hakurei",
+          variants: ["Alternate Color"]
+        }, {
+          name: "Marisa Kirisame",
+          variants: ["Alternate Color"]
+        }],
+        price: 6600,
+        type: "20cm",
+        MTO: true,
+      },
+      {
+        chars: ["Remilia Scarlet", "Flandre Scarlet", "Sakuya Izayoi"],
+        type: "yukkuri",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2025-10-20",
       end: "2025-11-25",
@@ -399,6 +719,23 @@ window.dummySales = [
     name: "Bunny & Others Sale",
     image: "/images/sales/G1Bhdv8aEAECLFR.jpg",
     desc: "40cm Alice & Patchy\nReisen\nTewi\nYuyuko\nYukari\nMini Reimu & Marisa",
+    merch: [
+      {
+        chars: ["Alice Margatroid", "Patchouli Knowledge"],
+        type: "40cm",
+        MTO: true,
+      },
+      {
+        chars: ["Reisen Udongein Inaba", "Tewi Inaba", "Yuyuko Saigyouji", "Yukari Yakumo"],
+        price: 6600,
+        type: "20cm",
+        MTO: true,
+      }, {
+        chars: ["Reimu Hakurei", "Marisa Kirisame"],
+        type: "mini",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2025-09-19",
       end: "2025-10-27",
@@ -413,6 +750,17 @@ window.dummySales = [
     name: "Green Bow Cirno Sale",
     image: "/images/sales/Guz_Wo4XwAETRIO.jpg",
     desc: "Green bow Cirno",
+    merch: [
+      {
+        chars: [{
+          name: "Cirno",
+          variants: ["Green Bow"]
+        }],
+        price: 6600,
+        type: "20cm",
+        MTO: true,
+      },
+    ],
     buying: {
       begin: "2025-07-04",
       end: "2025-09-01",
@@ -427,6 +775,13 @@ window.dummySales = [
     name: "Mini Plush (not MTO) Sale",
     image: "/images/sales/GqZgYXyWwAAlsAe.jpg",
     desc: "Suika\nRenko\nMaribel\nall 3 are NOT MTO",
+    merch: [
+      {
+        chars: ["Suika Ibuki", "Renko Usami", "Maribel Hearn"],
+        type: "mini",
+        MTO: false,
+      }
+    ],
     buying: {
       begin: "2025-05-07",
       end: "2025-06-09",
@@ -441,6 +796,34 @@ window.dummySales = [
     name: "Yuuma + Byak MTO Sale",
     image: "/images/sales/GqZO3W7XUAA1uWS.jpg",
     desc: "Lostword variants: Mokou, Sanae, Patchy\nByakuren\nYuuma\n40cm Sanae\n40cm Suwako\n40cm Cirno\n40cm Tanned Cirno\nSatori Yukkuri\nKoishi Yukkuri",
+    merch: [
+      {
+        chars: [{
+          name: "Fujiwara no Mokou",
+          variants: ["Lostword"]
+        }, {
+          name: "Sanae Kochiya",
+          variants: ["Lostword"]
+        }, {
+          name: "Patchouli Knowledge",
+          variants: ["Lostword"]
+        }, {
+        }, 'Byakuren Hijiri', 'Yuuma Toutetsu', ],
+        price: 6600,
+        type: "20cm",
+        MTO: true,
+      }, {
+        chars: ['Sanae Kochiya', 'Suwako Moriya', {
+          name: "Cirno",
+          variants: ["Regular", "Suntanned"]
+        }],
+        type: '40cm',
+        MTO: true,
+      }, {
+        chars: ['Satori Komeiji', 'Koishi Komeiji'],
+        type: 'yukkuri',
+      }
+    ],
     buying: {
       begin: "2025-05-07",
       end: "2025-06-09",
@@ -455,6 +838,14 @@ window.dummySales = [
     name: "MTO Resale",
     image: "/images/sales/GiDAQK9aoAAY-Ld.jpg",
     desc: "Kokoro\nRemilia (Kourindou)\nAlice\nTenshi",
+    merch: [
+      {
+        chars: ["Hata no Kokoro", "Remilia Scarlet", "Alice Margatroid", "Tenshi Hinanawi"],
+        price: 6600,
+        type: "20cm",
+        MTO: true,
+      }
+    ],
     buying: {
       begin: "2025-01-28",
       end: "2025-03-03",
